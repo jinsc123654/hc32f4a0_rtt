f6951420d21a784633beba32a2c13fe2a58f9502
jinsc <jinsc123654@gmail.com>
2024-06-19 21:07:12 +0800

    准备增加dap

--- applications/main.c
+++ applications/main.c
@@ -30,27 +30,12 @@ int main(void)
   
     rt_err_t ret = RT_EOK;
 
-    /* 设置日期 */
-    ret = set_date(2024, 6, 6);
-    if (ret != RT_EOK)
-    {
-        rt_kprintf("set RTC date failed\n");
-        return ret;
-    }
-    /* 设置时间 */
-    ret = set_time(12, 00, 00);
-    if (ret != RT_EOK)
-    {
-        rt_kprintf("set RTC time failed\n");
-        return ret;
-    }
-
     /* 挂载sd卡 */
     while(cnt++ < 10)
     {
         rt_thread_mdelay(200);
-        mkdir("/sdcard",0x777);
-        if(dfs_mount("sd","/sdcard","elm",0,0)==0)
+        //mkdir("/sdcard",0x777);
+        if(dfs_mount("sd","","elm",0,0)==0)
         {
             LOG_I("File System initialized");
             break;
@@ -65,14 +50,51 @@ int main(void)
             }
         }
     }
-    cnt = 0;
+    
+
+//    /* 设置日期 */
+//    ret = set_date(2024, 6, 6);
+//    if (ret != RT_EOK)
+//    {
+//        rt_kprintf("set RTC date failed\n");
+//        return ret;
+//    }
+//    /* 设置时间 */
+//    ret = set_time(12, 00, 00);
+//    if (ret != RT_EOK)
+//    {
+//        rt_kprintf("set RTC time failed\n");
+//        return ret;
+//    }
+
+//    /* 挂载sd卡 */
+//    while(cnt++ < 10)
+//    {
+//        rt_thread_mdelay(200);
+//        mkdir("/sdcard",0x777);
+//        if(dfs_mount("sd","/sdcard","elm",0,0)==0)
+//        {
+//            LOG_I("File System initialized");
+//            break;
+//        }
+//        else
+//        {
+//            LOG_E("File System init failed");
+//                      /* 格式化文件系统 */
+//            if (RT_EOK != dfs_mkfs("elm", "sd"))
+//            {
+//                LOG_E("Failed to Format fs!");
+//            }
+//        }
+//    }
+//    cnt = 0;
 
 
 //    extern void cdc_acm_init(uint8_t busid, uint32_t reg_base);
 //    cdc_acm_init(0, CM_USBFS_BASE);
 //    
-    extern void msc_storage_init(uint8_t busid, uint32_t reg_base);
-    msc_storage_init(0, CM_USBFS_BASE);
+//    extern void msc_storage_init(uint8_t busid, uint32_t reg_base);
+//    msc_storage_init(0, CM_USBFS_BASE);
 
     while (1)
     {
